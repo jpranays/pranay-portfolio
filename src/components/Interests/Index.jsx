ee8edae3db6a81edc77a6421a68c552f654df511
jpranays <pranay1315@gmail.com>
2023-02-06 03:41:17 +0530

    Added interests section

--- src/components/Interests/Index.jsx
+++ src/components/Interests/Index.jsx
@@ -1,8 +1,47 @@
 import React, { memo, useEffect, useRef } from "react";
-import { InterestsContainer } from "./Interests.styles";
+import {
+	InterestContent,
+	InterestDiv,
+	InterestImg,
+	InterestOverlay,
+	InterestsContainer,
+	InterestsHeaderTitle,
+	InterestsWrapper,
+} from "./Interests.styles";
 
 function Contact() {
-	return <InterestsContainer id="interests" />;
+	return (
+		<InterestsContainer id="interests">
+			<InterestsHeaderTitle>I like</InterestsHeaderTitle>
+			<InterestsWrapper>
+				<InterestDiv>
+					<InterestImg src="/running.webp" />
+					<InterestOverlay />
+					<InterestContent>Gym</InterestContent>
+				</InterestDiv>
+				<InterestDiv>
+					<InterestImg src="/bike-trips.webp" />
+					<InterestOverlay />
+					<InterestContent>Bike Trips</InterestContent>
+				</InterestDiv>
+				<InterestDiv>
+					<InterestImg src="/boxing.webp" />
+					<InterestOverlay />
+					<InterestContent>Boxing</InterestContent>
+				</InterestDiv>
+				<InterestDiv>
+					<InterestImg src="/inspecting.webp" />
+					<InterestOverlay />
+					<InterestContent>Inspecting Web Pages</InterestContent>
+				</InterestDiv>
+				<InterestDiv>
+					<InterestImg src="/ufc.webp" />
+					<InterestOverlay />
+					<InterestContent>UFC</InterestContent>
+				</InterestDiv>
+			</InterestsWrapper>
+		</InterestsContainer>
+	);
 }
 
 export default memo(Contact);
--- src/components/Interests/Interests.styles.js
+++ src/components/Interests/Interests.styles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const InterestsContainer = styled.div`
-	height: 100vh;
 	width: 100%;
 	background-color: ${({ theme }) => theme.body};
 	color: ${({ theme }) => theme.text};
@@ -9,4 +8,62 @@ export const InterestsContainer = styled.div`
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+`;
+export const InterestsWrapper = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+	align-items: center;
+	margin-top: 150px;
+	overflow: hidden;
+	@media screen and (max-width: 480px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+`;
+export const InterestsHeaderTitle = styled.h1`
+	font-size: 1.5rem;
+	color: ${({ theme }) => theme.text};
+	@media screen and (max-width: 480px) {
+		font-size: 1.2rem;
+		text-align: center;
+	}
+`;
+export const InterestDiv = styled.div`
+	width: 100%;
+	position: relative;
+	height: 250px;
+	border-radius: 10px;
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+	transition: all 0.5s ease;
+	&:hover {
+		transform: scale(1.1);
+	}
+`;
+export const InterestImg = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	position: absolute;
+	top: 0;
+	left: 0;
+	transition: all 0.5s ease;
+`;
+export const InterestOverlay = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	transition: all 0.5s ease;
+`;
+export const InterestContent = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	font-size: 1.5rem;
+	transition: all 0.5s ease;
+	text-align: center;
+	color: #fff;
 `;
